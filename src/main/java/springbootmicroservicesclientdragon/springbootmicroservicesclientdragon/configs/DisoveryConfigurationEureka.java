6f04d9f3e090d8a59232d1bd1af589caf4254531
gustavo9601 <tavo9601@gmail.com>
2022-09-09 00:46:49 -0500

    adding discovery eureka

---
+++ src/main/java/springbootmicroservicesclientdragon/springbootmicroservicesclientdragon/configs/DisoveryConfigurationEureka.java
@@ -0,0 +1,10 @@
+package springbootmicroservicesclientdragon.springbootmicroservicesclientdragon.configs;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableDiscoveryClient
+public class DisoveryConfigurationEureka {
+}
