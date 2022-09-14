b86bb62aaf1d5915024572d2bd426f45cc4b4f63
gustavo9601 <tavo9601@gmail.com>
2022-09-14 00:12:00 -0500

    add promethus

--- src/main/java/springbootmicroservicesclientdragon/springbootmicroservicesclientdragon/controllers/ApplicationNameController.java
+++ src/main/java/springbootmicroservicesclientdragon/springbootmicroservicesclientdragon/controllers/ApplicationNameController.java
@@ -1,18 +1,36 @@
 package springbootmicroservicesclientdragon.springbootmicroservicesclientdragon.controllers;
 
+import io.micrometer.core.annotation.Timed;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 import springbootmicroservicesclientdragon.springbootmicroservicesclientdragon.configs.DragonBallConfig;
 
 import java.time.LocalDateTime;
+import java.util.Random;
 import java.util.logging.Logger;
 
 @RestController
 @RequestMapping("/application-name")
 public class ApplicationNameController {
+
+    // Se inyecta para poder crear nuestra propia mestrica
+    @Autowired
+    private MeterRegistry meterRegistry;
+    /*
+     * Para consultar las metricas del ejemplo:
+     * http://localhost:8082/actuator/metrics/application-name-counter
+     *
+     * Para filtrar por el tag
+     * http://localhost:8082/actuator/metrics/application-name-counter?tag=<<name_tag>>:<<key_tag>>
+     * http://localhost:8082/actuator/metrics/application-name-counter?tag=level:low
+     * */
+
 
     @Autowired
     private DragonBallConfig dragonBallConfig;
@@ -21,7 +39,42 @@ public class ApplicationNameController {
 
     @GetMapping
     public ResponseEntity<String> getAppName() {
-        logger.info("Ejecutando getAppName a las :\t"+ LocalDateTime.now());
+        logger.info("Ejecutando getAppName a las :\t" + LocalDateTime.now());
+
+        // .increment() es para incrementar el contador, si se le pasa un parametro le sumara N al contador
+        int random = new Random().nextInt(100);
+        this.meterRegistry.counter("application-name-counter", // nombre de la metrica
+                "level",  // tag opcional para despues filtrar
+                (random > 50) ? "high" : "low"  // valor del tag
+        ).increment(random);
+
+        return ResponseEntity.ok(this.dragonBallConfig.getApplicationName());
+    }
+
+    /*
+     * Timed:
+     * http://localhost:8082/actuator/metrics/application-name-timed-get
+     *
+     * Añadira metricas de cantidad de peticiones, ultimo request en tiempo, y el maximo en demora, entre otros
+     * */
+    @Timed(value = "application-name-timed-get")
+    @GetMapping("/timed")
+    public ResponseEntity<String> getAppNameTimed() {
+        logger.info("Ejecutando getAppNameTimed a las :\t" + LocalDateTime.now());
+
+
+
+        Integer valueInteger = new Random().nextInt(100);
+        if (valueInteger < 50) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        } else if (valueInteger > 80) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Error");
+        }
+
         return ResponseEntity.ok(this.dragonBallConfig.getApplicationName());
     }
 }
